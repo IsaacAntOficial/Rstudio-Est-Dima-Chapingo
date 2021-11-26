ab4767127bdd6307097939c87dc135ea2e148819
IsaacAntOficial <89324432+IsaacAntOficial@users.noreply.github.com>
2021-11-26 17:51:21 -0600

    Add files via upload

---
+++ TAREA EXTRACLASE.R
@@ -0,0 +1,180 @@
+AREA EXTRACLASE NO. 2
+
+DEPARTAMENTO DE INGENIERÍA MECÁNICA AGRÍCOLA
+CURSO: MÉTODOS ESTADÍSTICOS
+TAREA EXTRACLASE NO. 2
+NOTA: ENTREGAR EN LOS EJERCICIOS DONDE SE PIDA EL USO DEL 
+SOFTWARE R, ADEMAS DE LA SOLUCIÓN ESCRITA, EL PROGRAMA Y 
+RESULTADO QUE SE OBTIENE DE APLICAR EL R.
+1. Suponga lo siguiente: la probabilidad de que un productor de Tabasco sea mayor de 
+40 años o tenga un tractor es de 0.4. Si la probabilidad de que sea mayor de 40 años 
+es de 0.2 y la probabilidad de que tenga un tractor es 0.3, ¿cuál es la probabilidad 
+de que sea mayor de 40 años y tenga un tractor?
+  2. En el ejercicio anterior defina los siguientes eventos:
+  A: productores mayor de 40 años
+B: productores con un tractor
+Calcule las probabilidades de los siguientes eventos:
+  a) A
+c
+
+b) A  B
+c
+
+c) (A  B)c
+
+d) A
+c  B 
+e) (B/A) 
+f) (B/Ac
+) 
+3. En una caja hay 10 baterías de las cuales 4 están en buen estado. Se repite dos veces 
+el siguiente ensayo: extraer una bacteria al azar, revisar su estado y devolverla a la 
+caja. 
+a. Encuentre la probabilidad que en ambos intentos se obtenga una batería en 
+buen estado.
+b. Calcule la probabilidad que en los dos intentos se obtenga al menos una batería 
+en buen estado.
+c. Supóngase que se extraen al azar dos baterías sin devolverlas a la caja. 
+Encuentre la probabilidad que:
+  - Ambas estén en buen estado; 
+- Solamente una esté en buen estado.
+4. Una fábrica tiene tres máquinas M1, M2, M3 para la producción de sus piezas. El 
+siguiente cuadro describe el porcentaje de producción diaria de cada una y la 
+frecuencia de piezas defectuosas que producen cada una.
+Máquina Producción (%) Piezas defectuosas (%)
+M1 50 4
+M2 30 3
+M3 20 2
+a) Determine la probabilidad que una pieza elegida al azar de la producción total del 
+día, sea defectuosa.
+b) Suponga que la pieza elegida al azar fue defectuosa. Determine la probabilidad 
+que haya sido producida por la Máquina M1.
+5. En un experimento se lanzan tres monedas y se observa el resultado. Suponga que 
+es de interés conocer el número de caras que se obtienen (X).
+a) Determine el espacio muestral y el valor que toma la variable aleatoria X para 
+cada elemento.
+b) Encuentre la distribución de probabilidad de la variable aleatoria X.
+c) Encuentre la distribución de probabilidad acumulada para X.
+6. Suponer que la probabilidad de éxito de un experimento es 0.2 y se realizan cinco 
+ensayos. Calcule la probabilidad que el primero y el último ensayo sean éxitos, y los 
+tres restantes sean fracasos. Suponer que los ensayos son independientes.
+7. Usando la tabla de probabilidades acumulativas de la distribución binomial que 
+aparece en los anexos de los libros de estadística calcular:
+  a) P(X  4) , P(X > 10) y P(8 < X  11) si X  Bin (18, 0.8)
+b) P(X  6), P(X < 2), P(X1) y P(3  X  5) si X  Bin (8, 0.55)
+c) P(X < 4), P(X = 3) si X  Bin (6, 0.1)
+Además, resolver el ejercicio utilizando el software R.
+8. Supóngase que se sabe que, en cierta región agrícola, el 10 por ciento de los 
+productores prefiere una marca de tractor particular para realizar sus labores 
+agrícolas. Si, de esta región, se extrae una muestra al azar de 25 productores, 
+determine la probabilidad de que:
+  a) Cinco o más prefieran esa marca de tractor.
+b) Cuatro o menos prefieren esa marca de tractor.
+c) Entre seis y nueve, prefieran esa marca de tractor.
+d) Dos, tres o cuatro prefieran esa marca de tractor.
+Además, resolver el ejercicio utilizando el software R.
+9. Una fábrica tiene una norma de control de calidad consistente en elegir al azar 
+diariamente 20 componentes electrónicos producidos y determinar el número de 
+unidades defectuosas. Si hay dos o más componentes defectuosos la fabricación se 
+detiene para inspección de los equipos. Se conoce por experiencia que la 
+probabilidad de que un componente producido sea defectuoso es 5 %. 
+a) Encuentre la probabilidad de que en cualquier día la producción se detenga al 
+aplicar esta norma de control de calidad.
+b) Encuentre la media y la varianza para esta distribución.
+10. En el estudio de ciertas algas para el tratamiento de aguas residuales, se tomaron 
+gran número de muestras de un estanque y se contó el número de algas que había 
+en cada muestra. Se encontró que el número promedio de algas por muestras era 
+de dos. Suponiendo que el número de algas está distribuido según Poisson, 
+encontrar la probabilidad de que la siguiente muestra que se tome contenga:
+  a) Una o más algas.
+b) Exactamente tres algas.
+c) Menos de cinco algas.
+Además, resolver el ejercicio utilizando el software R.
+11. Suponga que en todo el campo experimental de la UACh se siembra maíz y la 
+probabilidad de que las plantas de una parcela sean infectadas por cierta plaga a la 
+que están expuesta es 30 %, calcule la probabilidad que la décima parcela expuesta 
+a la infección sea la cuarta en contraerla.
+12. Una caja contiene 9 baterías de las cuales 4 están en buen estado y las restantes 
+defectuosas. Se toma una muestra eligiendo al azar tres baterías. Calcule la 
+probabilidad que en la muestra se obtengan:
+  a) Ninguna batería en buen estado.
+b) Al menos una batería en buen estado.
+c) No más de dos baterías en buen estado.
+Además, resolver el ejercicio utilizando el software R.
+13. La cantidad de errores de transmisión de datos en una hora es 5 en promedio. 
+Suponiendo que tiene distribución de Poisson, determine la probabilidad que:
+  a) En cualquier hora ocurra solamente un error.
+b) En cualquier hora ocurra al menos 3 errores.
+c) En dos horas cualesquiera ocurran no más de 2 errores.
+Además, resolver el ejercicio utilizando el software R.
+14. Utilizando la tabla de distribución normal estándar.
+a) Encontrar el área bajo la curva, entre 0 y 2.
+b) ¿Cuál es la probabilidad de que una z elegida al azar de entre la población de z 
+tenga un valor entre -2.55 y 2.55?
+  c) ¿Qué proporción de los valores de z están entre -2.74 y 1.53? 
+  d) Encontrar P(Z ≥ 2.71)
+e) Encontrar P(0.84 ≤ Z ≤ 2.45)
+15. La duración de un evento tiene distribución normal con media 10 y varianza 4. 
+Encuentre la probabilidad que el evento dure:
+  a) Menos de 9 horas
+b) Entre 11 y 12 horas
+16. En una planta empacadora de camarón se separa el producto en 6 calidades de 
+acuerdo con el peso del crustáceo como sigue:
+  Calidad Peso por pieza (gramos)
+A Más de 30
+B 25 a 30
+C 20 a 25
+D 16 a 20
+E 13 a 16
+F Menos de 13
+Suponga que el peso del camarón en el área de abasto de la planta puede modelarse 
+mediante una Normal con media de 22 gramos y desviación estándar de 6 gramos. Calcule 
+los porcentajes de cajas de cada calidad que produce la planta.
+Además, resolver el ejercicio utilizando el software R.
+17. Sea Z una variable aleatoria normal estándar. Usando la tabla correspondiente 
+encuentre:
+  P(Z < 0) = P(Z ≥ -1.49) =
+  P(Z < -0.28) = P(Z ≤ 2.14) =
+  P(Z < 0.28) = P(0.31 < Z < 0.78) =
+  P(Z ≤ 0.51) = P(-2.03 < Z < -1.27) =
+  P(Z > -0.49) = P(-0.77 < Z ≤ 1.03) =
+  18. Si Z˜N(0, 1), encuentre en cada caso el valor de c que satisfaga la ecuación.
+P(Z ≤ c) = 0.1131 P(0 < Z < c) = 0.0199
+P(Z > c) = 0.8869 P(c < Z < 0) = 0.4981
+P(Z > c) = 0.0073 P(-c < Z < c) = 0.7154
+P(Z ≥ c) = 0.5000 P(-c < Z < c) = 0.2434
+P(Z < c) = 0.9370 P(-c < Z < c) = 0.0802
+19. En las distribuciones Ji-cuadrada que siguen encuentre los valores de c que 
+satisfagan las igualdades.
+a) 𝑃(𝜒𝜐
+      2 ≥ 𝑐) = 0.01, 𝑝𝑎𝑟𝑎 𝜐 = 4, 𝜐 = 10, 𝜐 = 20, 𝜐 = 30
+      b) 𝑃(𝜒𝜐
+            2 ≥ 𝑐) = 0.005, 𝑝𝑎𝑟𝑎 𝜐 = 4, 𝜐 = 10, 𝜐 = 20, 𝜐 = 30
+            c) 𝑃(𝜒𝜐
+                  2 ≤ 𝑐) = 0.025, 𝑝𝑎𝑟𝑎 𝜐 = 6, 𝜐 = 15, 𝜐 = 25
+                  d) 𝑃(𝜒𝜐
+                        2 ≤ 𝑐) = 0.05, 𝑝𝑎𝑟𝑎 𝜐 = 6, 𝜐 = 15, 𝜐 = 25
+                        20. En las distribuciones t de Student que siguen encuentre los valores de c que 
+                        satisfagan las igualdades.
+                        a) 𝑃(𝑡𝜐 ≥ 𝑐) = 0.25, 𝑝𝑎𝑟𝑎 𝜐 = 1, 𝜐 = 20, 𝜐 = 60, 𝜐 = 120
+                        b) 𝑃(𝑡𝜐 ≥ 𝑐) = 0.005, 𝑝𝑎𝑟𝑎 𝜐 = 1, 𝜐 = 5, 𝜐 = 10, 𝜐 = 40
+                        c) 𝑃(𝑡𝜐 ≤ 𝑐) = 0.025, 𝑝𝑎𝑟𝑎 𝜐 = 4, 𝜐 = 12, 𝜐 = 18, 𝜐 = 29
+                        21. En las distribuciones de F que siguen encuentre los valores de c que satisfagan las 
+                        igualdades
+                        a) 𝑃(𝐹𝑛
+                              𝑚 ≥ 𝑐) = 0.05; 𝑝𝑎𝑟𝑎 𝑚 = 3, 𝑛 = 2; 𝑚 = 8, 𝑛 = 8; 𝑚 = 15, 𝑛 = 13; 𝑚 =
+                                20, 𝑛 = 24
+                                b) 𝑃(𝐹𝑛
+                                      𝑚 ≥ 𝑐) = 0.10; 𝑝𝑎𝑟𝑎 𝑚 = 5, 𝑛 = 4; 𝑚 = 10, 𝑛 = 10; 𝑚 = 12, 𝑛 = 22
+                                      22. El porcentaje de proteína (X) en una variedad de soya tiene una media de 23, con 
+                                      una desviación estándar de 3. Se realizan 10 determinaciones independientes del 
+                                      contenido de proteína en dicha variedad. Suponga que X tiene una distribución 
+                                      Normal
+                                      a) ¿Cuál es la distribución de 𝑋̅, la media aritmética de las determinaciones?
+  b) Cuál es la probabilidad de que la 𝑋̅ que se obtenga sea mayor de 24?¿De que 
+esté entre 22.5 y 23.5?
+  c) Suponga ahora que se hacen 50 determinaciones. Calcule las mismas 
+probabilidades que en el inciso b)
+d) Calcule un intervalo de confianza del 90 % de probabilidad para la media 
+poblacional, si se conoce que la muestra tiene una 𝑋̅= 31 y el tamaño de la 
+población es de 100.
